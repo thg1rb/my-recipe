11753aff7971e36e0d0acd9957fd08db83128efc
Bowornrat <bowornrat.t@ku.th>
2025-03-27 01:34:41 +0700

    The slide animation for Main Screen which is in bottom nav

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -6,8 +6,15 @@ import 'package:my_recipe/screens/home_screen.dart';
 import 'package:my_recipe/screens/profile_screen.dart';
 import 'package:my_recipe/widgets/navigation_bar/bottom_navbar.dart';
 
-class MainScreen extends ConsumerWidget {
-  MainScreen({super.key});
+class MainScreen extends ConsumerStatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends ConsumerState<MainScreen> {
+  final PageController _pageController = PageController();
 
   final List<Widget> bottomNavbarScreens = <Widget>[
     HomeScreen(),
@@ -16,18 +23,37 @@ class MainScreen extends ConsumerWidget {
   ];
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     int bottomNavbarIndex = ref.watch(bottomNavbarIndexProvider);
+
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 15),
-        child: bottomNavbarScreens[bottomNavbarIndex],
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            ref.read(bottomNavbarIndexProvider.notifier).state = index;
+          },
+          physics: const NeverScrollableScrollPhysics(),
+          children: bottomNavbarScreens, // Disable manual swipe
+        ),
       ),
       bottomNavigationBar: BottomNavbar(
         index: bottomNavbarIndex,
-        onChangeIndex:
-            (index) =>
-                ref.read(bottomNavbarIndexProvider.notifier).state = index,
+        onChangeIndex: (index) {
+          ref.read(bottomNavbarIndexProvider.notifier).state = index;
+          _pageController.animateToPage(
+            index,
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeInOut,
+          );
+        },
       ),
     );
   }
